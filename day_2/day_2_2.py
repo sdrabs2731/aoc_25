231b2be0cb897efa5d72ea08a599bade769813d7
Stephen Lee Bard <sjlbard@gmail.com>
2025-12-02 21:36:52 -0500

    final version

--- day_2/day_2_2.py
+++ day_2/day_2_2.py
@@ -1,60 +1,82 @@
-"""AOC 2025 Day 2"""
+"""AOC 2025 - Day 2"""
+
+from math import isqrt
 
 def read_input(filename: str):
     """Read input"""
     with open(filename, 'r', encoding="utf_8") as file:
-        raw_data = file.readlines()
-        data = []
-        for line in raw_data:
-            data.append(line.strip().split(','))
-        return data[0]
+        line = file.readline().strip()
+        return line.split(',')
 
-def split_even(str_number, factor):
-    return [str_number[idx:idx + factor] for idx in range(0, len(str_number), factor)]
+def get_factors(num_digits):
+    """Compute all positive factors of 'num_digits' excluding the number itself"""
 
-def count_invalid(data):
-    "Count number of invalid entries in given ranges"
+    factors = []
+    limit = isqrt(num_digits)  # us isqrt to avoid floats
 
-    count = 0
-    for item in data: 
-        item = item.split('-')
-        minimum = int(item[0])
-        maximum = int(item[1])
-    
-        for number in range(minimum, maximum + 1):
-            factors = []
-            str_number = str(number)
+    # Check each integer from 1 to sqrt(number)
+    for idx in range(1, limit + 1):
+        if num_digits % idx == 0:
+            factors.append(idx)
 
-            for idx in range(1, int((len(str_number)**0.5) + 1)):
-                if len(str_number) % idx == 0:
-                    factors.append(idx)
-                    if idx * idx != len(str_number):
-                        factors.append(len(str_number) // idx)
-            factors.sort()
-            #print(number, factors)
-            
-            invalid = False
-            for factor in factors:
-                if invalid is True:
-                    break
-                split_string = split_even(str_number, factor)
-                #print(split_string)
-                if len(split_string) != 1:
-                    if len(set(split_string)) == 1:
-                        #print(number)
-                        count += number
-                        invalid = True
-    return count
+            # Add paired factor
+            if idx * idx != num_digits:
+                factors.append(num_digits // idx)
+
+    factors.sort()
+
+    # remove factor equal to number digits
+    if factors and factors[-1] == num_digits:
+        factors.pop()
+
+    return factors
+
+def split_groups(str_number, factor):
+    "Create list of all possible groups"
+
+    groups = []
+    num_digits = len(str_number)
+
+    # Iterate by group size
+    for idx in range(0, num_digits, factor):
+        groups.append(str_number[idx:idx + factor])
+
+    return groups
+
+def is_invalid(str_number, factors):
+    """Calculate solution"""
+
+    for factor in factors:
+        split_string = split_groups(str_number, factor)
+
+        # Test if all groups are the same
+        if len(set(split_string)) == 1:
+            return True
+
+    return False
 
 def main():
     """Entry point"""
-    filename = "test_data.txt"
+    solution = 0
     filename = "data_2.txt"
-
     data = read_input(filename)
-    invalid = count_invalid(data)
 
-    print(invalid)
+    for item in data:
+        parts = item.split('-')
+        minimum = int(parts[0])
+        maximum = int(parts[1])
+
+        for number in range(minimum, maximum + 1):
+            str_number = str(number)
+            num_digits = len(str_number)
+
+            factors = get_factors(num_digits)
+
+            # If number is invalid, add to solution total
+            if is_invalid(str_number, factors):
+                solution += number
+
+    print(solution)
 
 if __name__ == "__main__":
     main()
