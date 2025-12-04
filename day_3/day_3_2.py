f3946626f5f7e8c88bb11a376a3ef2f629496c78
Stephen Lee Bard <sjlbard@gmail.com>
2025-12-03 19:24:12 -0500

    minor formating changes and type hinting

--- day_3/day_3_2.py
+++ day_3/day_3_2.py
@@ -1,21 +1,26 @@
 """AOC 2025 - Day 3, Part 1 and Part 2"""
 
-def read_input(file_path: str) -> str:
-    """Reads the input file and returns the content as a string."""
+from typing import List
 
-    data = []
+
+def read_input(file_path: str) -> List[str]:
+    """Reads the input file and returns the content as a list of strings."""
+
+    data: List[str] = []
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
             data.append(line.strip())
+
     return data
 
-def compute_solution(data: str, keep) -> int:
-    """Computes the solution for Day 3, Part 2."""
-    solution = 0
+
+def compute_solution(data: List[str], keep: int) -> int:
+    """Computes the solution for Day 3, Parts 1 and 2."""
+    solution: int = 0
 
     for line in data:
-        to_remove = len(line) - keep  # number of digits to remove
-        stack = []
+        to_remove: int = len(line) - keep  # number of digits to remove
+        stack: List[str] = []
 
         for digit in line:
             # Remove smaller digits from stack if to_remove > 0
@@ -27,21 +32,23 @@ def compute_solution(data: str, keep) -> int:
         # If there are still digits to remove, pop from the end of the stack
         if to_remove > 0:
             for _ in range(to_remove):
-                stack.pop(  )
+                stack.pop()
 
-        solution += int(''.join(stack[:keep]))
+        solution += int(''.join(stack))
 
     return solution
 
 
-def main():
+def main() -> None:
     """Main function to execute the solution."""
-    input_data = read_input('test_3.txt')
 
-    part_one_solution = compute_solution(input_data, 2)
-    part_two_solution = compute_solution(input_data, 12)
+    input_data: List[str] = read_input('test_3.txt')
 
-    print(f"Part 1: {part_one_solution}\nPart 2: {part_two_solution}")
+    part_one: int = compute_solution(input_data, 2)
+    part_two: int = compute_solution(input_data, 12)
+
+    print(f"Part 1: {part_one}\nPart 2: {part_two}")
+
 
 if __name__ == "__main__":
     main()
