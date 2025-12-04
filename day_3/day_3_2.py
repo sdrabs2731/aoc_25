cc9ab29e5058c06089344eb47cbc5f947d9b9ba9
Stephen Lee Bard <sjlbard@gmail.com>
2025-12-03 19:09:44 -0500

    combined part 1 and 2

--- day_3/day_3_2.py
+++ day_3/day_3_2.py
@@ -1,15 +1,15 @@
-"""AOC 2025 - Day 3, Part 2"""
+"""AOC 2025 - Day 3, Part 1 and Part 2"""
 
 def read_input(file_path: str) -> str:
     """Reads the input file and returns the content as a string."""
 
     data = []
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
             data.append(line.strip())
     return data
 
-def compute_solution(data: str, keep=12) -> int:
+def compute_solution(data: str, keep) -> int:
     """Computes the solution for Day 3, Part 2."""
     solution = 0
 
@@ -23,7 +23,7 @@ def compute_solution(data: str, keep=12) -> int:
                 stack.pop()
                 to_remove -= 1
             stack.append(digit)  # Push the current digit onto the stack
-        
+
         # If there are still digits to remove, pop from the end of the stack
         if to_remove > 0:
             for _ in range(to_remove):
@@ -36,9 +36,12 @@ def compute_solution(data: str, keep=12) -> int:
 
 def main():
     """Main function to execute the solution."""
-    input_data = read_input('data_3.txt')
-    solution = compute_solution(input_data)
-    print(solution)
-    
+    input_data = read_input('test_3.txt')
+
+    part_one_solution = compute_solution(input_data, 2)
+    part_two_solution = compute_solution(input_data, 12)
+
+    print(f"Part 1: {part_one_solution}\nPart 2: {part_two_solution}")
+
 if __name__ == "__main__":
     main()
