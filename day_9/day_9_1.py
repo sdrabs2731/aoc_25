b099a3045eb67c6b7c3709e3d20daf3cceb62ad3
Stephen Lee Bard <sjlbard@gmail.com>
2025-12-09 11:12:17 -0500

    day 9 part 1 solution

---
+++ day_9/day_9_1.py
@@ -0,0 +1,45 @@
+"""AOC 2025 - Day 9 - Part 1"""
+
+from typing import List
+
+
+def read_input(file_path: str) -> List[tuple]:
+    data: List[tuple] = []
+    with open(file_path, "r", encoding="utf-8") as file:
+        for line in file:
+            data.append(tuple(int(x) for x in line.strip().split(',')))
+    return data
+
+
+def calculate_area(point_one, point_two) -> int:
+    """Return area of rectangle formed from two opposite corner points"""
+
+    length: int = abs(point_one[0] - point_two[0]) + 1
+    height: int = abs(point_one[1] - point_two[1]) + 1
+  
+    return length * height
+
+
+def process_data(data) -> int:
+    """Process all point and return largest area"""
+
+    max_area: int = 0
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            area = calculate_area(data[i], data[j])
+            if area > max_area:
+                max_area = area
+
+    return max_area
+
+
+def main() -> None:
+    """Execute solution"""
+
+    data = read_input("data_9.txt")
+    solution = process_data(data)
+    print(f"Solution: {solution}")    
+
+
+if __name__ == "__main__":
+    main()
